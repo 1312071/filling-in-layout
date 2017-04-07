02dc9f59e2d755ff57a2862f84f5816da5226db1
1312071 <1312071@student.hcmus.edu.vn>
2017-04-07 16:35:05 +0700

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   root "static_pages#home"
   post "/signup", to: "users#create"
   resources :users
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
